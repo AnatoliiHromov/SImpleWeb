f93a2c7601d4f1379539364fb673cb37e3871bd8
Gromov <a.gromov360@gmail.com>
2019-06-24 22:03:46 +0300

    break down test2 and change displayed name

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
+    String someone = "Worl1d";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
